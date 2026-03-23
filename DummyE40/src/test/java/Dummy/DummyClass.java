5731e11fe9b655d311e4c71abadd863f1ffdfdc2
geet7785 <geet.pri7785@gmail.com>
2026-03-23 17:24:58 +0530

    commit

---
+++ DummyE40/src/test/java/Dummy/DummyClass.java
@@ -0,0 +1,12 @@
+package Dummy;
+
+public class DummyClass {
+
+	public static void main(String[] args) {
+		System.out.println("hellooooo");
+		System.out.println("hellooooo");
+		System.out.println("hellooooo");
+
+	}
+
+}
